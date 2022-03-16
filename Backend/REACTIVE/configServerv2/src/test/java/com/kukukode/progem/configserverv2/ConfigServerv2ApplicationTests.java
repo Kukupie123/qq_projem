72df12bdd8f599f805a78ddb3048ef3e5289e75e
kukupie123 <kukuborom@gmail.com>
2022-03-16 21:59:38 +0530

    Added centralized configuration server

---
+++ Backend/REACTIVE/configServerv2/src/test/java/com/kukukode/progem/configserverv2/ConfigServerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kukukode.progem.configserverv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
