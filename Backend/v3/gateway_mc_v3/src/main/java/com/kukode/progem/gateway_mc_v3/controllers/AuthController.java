1ba72bf523a0bfe77f8a78924545b10e3c945026
kukupie123 <kukuborom@gmail.com>
2022-03-20 14:01:37 +0530

    Basic changes

--- Backend/v3/gateway_mc_v3/src/main/java/com/kukode/progem/gateway_mc_v3/controllers/AuthController.java
+++ Backend/v3/gateway_mc_v3/src/main/java/com/kukode/progem/gateway_mc_v3/controllers/AuthController.java
@@ -3,6 +3,10 @@ package com.kukode.progem.gateway_mc_v3.controllers;
 import com.kukode.progem.gateway_mc_v3.models.BaseResponse;
 import com.kukode.progem.gateway_mc_v3.models.requests.SignInUp;
 import com.kukode.progem.gateway_mc_v3.services.AuthService;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +17,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+    final Logger log = LoggerFactory.getLogger("Auth Controller Logger");
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -20,14 +25,27 @@ public class AuthController {
     }
 
     /**
-     * @param body email and cred
+     * @param body Email and Cred
      * @return true or false as data and 200 ok status code when successful
      */
     @PostMapping("/sign-up")
     public Mono<ResponseEntity<BaseResponse<Boolean>>> signUp(@RequestBody SignInUp body) {
         //TODO: Custom Exception viewing
+        log.debug("Sign-up request triggered");
         System.out.println("Sign-up endpoint triggered");
         return authService.signUp(body);
 
+    }
+
+    /**
+     * Communicates with Auth MC to retrieve JWT token based on payload
+     *
+     * @param body Email and Cred
+     * @return JWT token on successful authentication
+     */
+    @PostMapping("/sign-in")
+    public Mono<ResponseEntity<BaseResponse<String>>> signIn(@RequestBody SignInUp body) {
+        log.debug("Sign-in request triggered");
+        return null;
     }
 }
